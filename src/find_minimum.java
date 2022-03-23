bdf45859b1bf11e49a2c8fb2ced4c0996cdf1435
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-03-23 14:41:38 +0700

    update

--- src/find_minimum.java
+++ src/find_minimum.java
@@ -3,7 +3,7 @@ public class find_minimum {
     public static void main(String[] args) {
         int[] arr ={1,2,3,4,5,6};
         int index = minValue(arr);
-        System.out.println("The smallest element in the array is :");
+        System.out.println("The smallest element in the array is :" +arr[index]);
     }
     public static int minValue(int[]array) {
         int index = 0;
